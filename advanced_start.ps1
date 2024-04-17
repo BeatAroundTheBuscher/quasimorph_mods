3b8bfd1494f386303d6ceca8e99ff59da157a4ea
Buscher <beataroundthebuscher@protonmail.com>
2024-04-17 16:46:04 +0200

    First Version
    Read and find correct lines, compare with lines from CSV

---
+++ advanced_start.ps1
@@ -0,0 +1,89 @@
+function Backup-File {
+
+    if ( [System.IO.File]::Exists($backupPath) )
+    {
+        $overwriteBackup = Read-Host "Do you want to overwrite your backup? (y to proceed, anything else to abort)"
+        if ( $overwriteBackup -match "y" )
+        {
+            Write-Output "Overwriting backup in path $backupPath"
+        }
+        else
+        {
+            Write-Output "Not overwriting backup - Stopping here"
+            exit
+        }
+    }
+}
+
+
+# There are odd values (f. ex. 127) for line length so maybe padding isn't needed after all
+function Line-Padding-Alignment-4 {
+
+    param (
+        $line
+    )
+
+    Write-Output $line
+    Write-Output $line.Length
+
+}
+
+
+
+
+# front or back slash depends on OS
+
+if ( $IsLinux -eq $true )
+{
+    $originalFilePath = "$PWD/resources.assets"
+    $csvFilePath = "$PWD/advanced_start.csv"
+
+}
+else
+{
+    $originalFilePath = "$PWD\resources.assets"
+    $csvFilePath = "$PWD\advanced_start.csv"
+}
+
+$backupPath = "$originalFilePath.bak"
+# Backup-File
+
+
+
+
+cp $originalFilePath $backupPath
+
+# $content = [System.IO.File]::ReadAllBytes($originalFilePath)
+# $content = [System.IO.File]::ReadAllLines($originalFilePath)
+
+$streamReader = [System.IO.File]::OpenText($originalFilePath)
+$streamReaderCSV = [System.IO.File]::OpenText($csvFilePath)
+$lineFound = $false
+
+while ( ($line = $streamReader.ReadLine()) -ne $null)
+{
+    if ( $lineFound -eq $false -and $line -match "InitialPower.*InitialTechLevel" )
+    {
+        $lineFound = $true
+        $lineNumber = 0 # index start at 0
+    }
+    if ( $lineFound)
+    {
+        $csvLine = $streamReaderCSV.ReadLine()
+        Write-Output "$csvLine"
+        Write-Output "$line"
+        # Line-Padding-Alignment-4 $line
+        $lineNumber += 1
+        Write-Output "$lineNumber"
+    }
+    if ( $lineFound -eq $true -and $line -match "#end" )
+    {
+        $lineFound = $false
+        exit
+    }
+
+}
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+## How to Use
+
+- Move resources.assets into the same folder
+- modify advanced_start.csv (For now only Column C - InitialPower and D - InitialTechLevel)
+- Run (in Linux) with `pwsh advanced_start.ps1`
