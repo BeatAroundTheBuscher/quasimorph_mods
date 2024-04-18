6236e74ee5476a234de2bcddf8102902910efc53
Buscher <beataroundthebuscher@protonmail.com>
2024-04-18 16:52:27 +0200

    Solution 2 Step 1

--- advanced_start/advanced_start.ps1
+++ advanced_start/advanced_start.ps1
@@ -16,20 +16,6 @@ function Backup-File {
 }
 
 
-# There are odd values (f. ex. 127) for line length so maybe padding isn't needed after all
-function Line-Padding-Alignment-4 {
-
-    param (
-        $line
-    )
-
-    Write-Output $line
-    Write-Output $line.Length
-
-}
-
-
-
 $originalFileName = "resources.assets"
 $csvFileName = "advanced_start.csv"
 
@@ -37,25 +23,123 @@ $originalFilePath = Join-Path $PWD $originalFileName
 $csvFilePath = Join-Path $PWD $csvFileName
 
 $backupPath = "$originalFilePath.bak"
+
 # Backup-File
+
 Write-Output "$originalFilePath"
 Write-Output "$csvFilePath"
 
-
-
 cp $originalFilePath $backupPath
 
-# $content = [System.IO.File]::ReadAllBytes($originalFilePath)
-# $content = [System.IO.File]::ReadAllLines($originalFilePath)
-# $content.length
+
+$fileContent = Get-Content -Raw -Path $originalFilePath
+
+# 11801008 # nail	Enabled	# 0xb411b0 but should start at b411bd 
+# it's possible that pattern matching is only 16 bytes accurate
+
+# 11026072 = 0xA83E98 but should be 0xB41280 - difference is 0xBD3E8 = 775144
+$searchPattern = 'MinQmorphosWhenVictims' 
+
+$match = [regex]::Match($fileContent, $searchPattern)
 
 
-## Chat GPT
+if ($match.Success) {
+    # Get the index of the match within the file content
+    $matchIndex = $match.Index + 775144 # no clue why this offset is necessary
+
+    $fileStream = [System.IO.File]::Open($originalFilePath, 'Open', 'ReadWrite')
+    $fileStream.Seek($matchIndex, 'Begin')
+
+    $bytesToRead = 30
+    $bytesRead = @()
+
+    # find factions after column name row (past \x0a which is \n)
+
+    $buffer = New-Object byte[] $bytesToRead
+    $bytesReadInThisIteration = $fileStream.Read($buffer, 0, $bytesToRead)
+
+    for ( ($i = 0); $i -lt $bytesToRead; $i++  )
+    {
+        $hexValue = [System.BitConverter]::ToString($buffer[$i]) -replace '-', ' '
+        #$hexValue
+
+        if ( $hexValue -match "0A")
+        {
+            break
+        }
+    }
+
+    # factions start here
 
 
-# Read the content of the file into an array of lines
-$lines = Get-Content -Path $originalFilePath
-$lines | Set-Content -Path $originalFilePath
+    
+
+    do 
+    {
+        $factionStart = $matchIndex + $i + 1
+        $fileStream.Seek($factionStart, 'Begin')
+        $bytesToRead = 150
+        $bytesRead = @()
+        $buffer = New-Object byte[] $bytesToRead
+        $bytesReadInThisIteration = $fileStream.Read($buffer, 0, $bytesToRead)
+
+        for ( ($i = 0); $i -lt $bytesToRead; $i++  )
+        {
+            $hexValue = [System.BitConverter]::ToString($buffer[$i]) -replace '-', ' '
+            #$hexValue
+
+            if ( $hexValue -match "0A")
+            {
+                Write-Output "Very bueno @ $i"
+                break
+            }
+        }
+        $testbuffer = [System.Text.Encoding]::UTF8.GetString($buffer)
+        $testbuffer
+        $matchIndex = $factionStart
+        $bla++;
+    # } while ($bla -lt 20)
+    } while (-not ($testbuffer -match "end"))
+
+
+    exit
+
+
+    while ($bytesRead.Count -lt $bytesToRead) {
+        # Calculate the remaining bytes to read
+        $remainingBytes = $bytesToRead - $bytesRead.Count
+        
+        # Create a byte array to store the bytes read in this iteration
+        $buffer = New-Object byte[] $remainingBytes
+
+        # Read bytes from the file into the buffer
+        $bytesReadInThisIteration = $fileStream.Read($buffer, 0, $remainingBytes)
+
+        # Check if any bytes were read in this iteration
+        if ($bytesReadInThisIteration -eq 0) {
+            # End of file reached or no bytes available
+            break
+        }
+
+        # Append the bytes read in this iteration to the array of bytes read
+        $bytesRead += $buffer[0..($bytesReadInThisIteration - 1)]
+        $hexString = [System.BitConverter]::ToString($buffer) -replace '-', ' '
+        $bytesRead.Length
+        $hexString
+    }
+
+    
+
+    # $Mybytes = [byte[]]
+    # $fileStream.ReadExactly($MyBytes, $matchIndex, 10)
+    # $MyBytes
+}
+else
+{
+    Write-Output "No bueno"
+    exit
+}
+
 
 exit
 
